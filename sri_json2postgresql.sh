d21a2ed18178558a8c9f4ab34edc15f351d93c0f
Terror <miguel.a.terron@gmail.com>
2019-07-14 20:18:50 +1200

    Cleanup DB import script and parsing script. FINAL version.

--- sri_json2postgresql.sh
+++ sri_json2postgresql.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
-# You need to have $PGPASSWORD set for this to work
+SCRIPT_NAME=$(basename -- "$0")
+
+logi() {
+	printf "%s [INFO] ${SCRIPT_NAME}: %s\n" "$(date -Iseconds)" "$@" >&2
+}
+
 set -e
 
 import_to_db () {
 	if [ -f /sri/import-ready ]; then
-		echo 'Importing to DB' >&2
-		psql -U "${POSTGRES_USER:=postgres}" -h "${POSTGRES_HOST:=postgres}" -c "COPY sri (data) FROM STDIN;" < /sri/SRI.json
+		logi 'Importing to DB'
+		# You need to have $PGPASSWORD set for this to work
+		logi "$(psql -U "${POSTGRES_USER:=postgres}" -h "${POSTGRES_HOST:=postgres}" -c "COPY sri (data) FROM STDIN;" < /sri/SRI.json) records imported"
 		mv /sri/SRI.json /sri/SRI.json.bak
 	fi
 }
 
-import_to_db
+
+if [ $PGPASSWORD ]; then
+	import_to_db
+else
+	logi '$PGPASSWORD not set, can'"'"'t connect to DB'
+	exit 1
+fi
