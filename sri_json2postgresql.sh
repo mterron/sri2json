0ea702fc1b663ffa022c863ce940ea7f808cacaf
mterron <miguel.a.terron@gmail.com>
2019-07-15 11:15:12 +1200

    Add helpful error when libpq env variables are not set

--- sri_json2postgresql.sh
+++ sri_json2postgresql.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# You need to have $PGUSER, $PGHOST & $PGPASSWORD set for this script to work
 SCRIPT_NAME=$(basename -- "$0")
 
 logi() {
@@ -10,16 +11,16 @@ set -e
 import_to_db () {
 	if [ -f /sri/import-ready ]; then
 		logi 'Importing to DB'
-		# You need to have $PGPASSWORD set for this to work
-		logi "$(psql -U "${POSTGRES_USER:=postgres}" -h "${POSTGRES_HOST:=postgres}" -c "COPY sri (data) FROM STDIN;" < /sri/SRI.json) records imported"
+		# psql uses the lipq env variable to define username, password and host
+		logi "$(psql -c "COPY sri (data) FROM STDIN;" < /sri/SRI.json) records imported"
 		mv /sri/SRI.json /sri/SRI.json.bak
 	fi
 }
 
-
-if [ $PGPASSWORD ]; then
+if [ $PGUSER ] && [ $PGHOST ] && [ $PGPASSWORD ]; then
 	import_to_db
 else
-	logi '$PGPASSWORD not set, can'\''t connect to DB'
+	logi 'You must set $PGUSER, $PGPASSWORD and $PGHOST to connect to PostgreSQL'
 	exit 1
 fi
+
