f79f120e47f1ec398ea5c6a55382d9e99589e461
Terror <miguel.a.terron@gmail.com>
2019-08-13 08:45:12 +1200

    Rename json key (component -> name)

--- sri2json.sh
+++ sri2json.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 SCRIPT_NAME=$(basename -- "$0")
 
+logi() {
+	printf "%s [INFO] ${SCRIPT_NAME}: %s\n" "$(date -Iseconds)" "$@" >&2
+}
+
 logd() {
 	if [ "${DEBUG:-0}" = 1 ]; then
-		printf "%s [INFO] ${SCRIPT_NAME}: %s\n" "$(date -Iseconds)" "$@" >&2
+		printf "%s [DEBUG] ${SCRIPT_NAME}: %s\n" "$(date -Iseconds)" "$@" >&2
     fi
 }
 
 process_sri_tree() {
 	cd "${WORKDIR}"/new-website
 	logd 'Processing CDNJS SRI tree'
-	git --no-pager log --pretty="format:" --name-only --since="$(date -Iseconds -r /sri/last-run)" -- sri | sort -u | xargs -r -n1 -P1 jq --unbuffered -M '{lang: "js", component: (input_filename|split("/"))[1], version: ((input_filename|split("/"))[2]|sub(".json"; "")), hashes: [ to_entries[] | {file: .key, (.value|scan("(.*)-")[]): (.value|sub("(.*-)"; "")) } ] }' 2>/dev/null | awk -F: -v decode_sha256="base64 -d - 2>/dev/null | xxd -p -c32 | tr -cd '[:xdigit:]'" '{ if ( $1 ~ "sha256") { ORS="";print $1": \""; print $2 | decode_sha256; close(decode_sha256); print "\"\n";ORS="\n" } else print $0 }' | jq -M -c '.' >/sri/SRI.json
+	git --no-pager log --pretty="format:" --name-only --since="$(date -Iseconds -r /sri/last-run)" -- sri | sort -u | xargs -r -n1 -P1 jq --unbuffered -M '{lang: "js", name: (input_filename|split("/"))[1], version: ((input_filename|split("/"))[2]|sub(".json"; "")), hashes: [ to_entries[] | {file: .key, (.value|scan("(.*)-")[]): (.value|sub("(.*-)"; "")) } ] }' 2>/dev/null | awk -F: -v decode_sha256="base64 -d - 2>/dev/null | xxd -p -c32 | tr -cd '[:xdigit:]'" '{ if ( $1 ~ "sha256") { ORS="";print $1": \""; print $2 | decode_sha256; close(decode_sha256); print "\"\n";ORS="\n" } else print $0 }' | jq -M -c '.' >/sri/SRI.json
 	logd 'Processing SRI tree done'
 	touch -d "$(date -d "@$(git log -1 --format=%ct)" +%Y%m%d%H%M.%S)" /sri/last-run
 	touch /sri/import-ready
@@ -28,13 +32,13 @@ fi
 
 # Update sri tree from git repo
 if [ -d new-website/ ]; then
-	logd 'Updating CDNJS "new-website" repo'
+	logi 'Updating CDNJS "new-website" repo'
 	cd "${WORKDIR}"/new-website/
 	if ! [  "$(git pull)" = 'Already up to date.' ]; then
 		process_sri_tree
 	fi
 else
-	logd 'Cloning CDNJS "new-website" repo'
+	logi 'Cloning CDNJS "new-website" repo'
 	git clone -q https://github.com/cdnjs/new-website.git
 	cd "${WORKDIR}"/new-website
 	# git configuration
