81c633596fe54e7255850aaecc8a02dbdc8741f2
Terror <miguel.a.terron@gmail.com>
2019-08-14 12:55:41 +1200

    Improve error condition checking

--- SRI_to_dtrack.sh
+++ SRI_to_dtrack.sh
@@ -3,6 +3,9 @@
 mkdir -p /sri
 while true; do
 	sri2json.sh
-	json2postgresql.sh /sri/SRI.json
+	if [ -f /sri/import-ready ]; then
+		json2postgresql.sh /sri/SRI.json
+	fi
+	rm -f /sri/import-ready
 	sleep 1h
 done
--- sri2json.sh
+++ sri2json.sh
@@ -17,7 +17,9 @@ process_sri_tree() {
 	git --no-pager log --pretty="format:" --name-only --since="$(date -Iseconds -r /sri/last-run)" -- sri | sort -u | xargs -r -n1 -P1 jq --unbuffered -M '{lang: "js", name: (input_filename|split("/"))[1], version: ((input_filename|split("/"))[2]|sub(".json"; "")), hashes: [ to_entries[] | {file: .key, (.value|scan("(.*)-")[]): (.value|sub("(.*-)"; "")) } ] }' 2>/dev/null | awk -F: -v decode_sha256="base64 -d - 2>/dev/null | xxd -p -c32 | tr -cd '[:xdigit:]'" '{ if ( $1 ~ "sha256") { ORS="";print $1": \""; print $2 | decode_sha256; close(decode_sha256); print "\"\n";ORS="\n" } else print $0 }' | jq -M -c '.' >/sri/SRI.json
 	logd 'Processing SRI tree done'
 	touch -d "$(date -d "@$(git log -1 --format=%ct)" +%Y%m%d%H%M.%S)" /sri/last-run
-	touch /sri/import-ready
+	if ! [ "$(head -1 /sri/SRI.json | wc -c)" -eq 0 ]; then
+		touch /sri/import-ready
+	fi
 }
 
 set -e
